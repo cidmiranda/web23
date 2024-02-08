045f6970b4a7fad96b5056a2940edd0381af8b5a
Cid Miranda <cid.miranda@americanas.io>
2024-02-08 09:30:05 -0300

    curso web3 modulo 1

---
+++ protochain/__tests__/blockchain.test.ts
@@ -0,0 +1,66 @@
+import { describe, test, expect, jest } from '@jest/globals';
+import Blockchain from '../src/lib/blockchain';
+import Block from '../src/lib/block';
+
+jest.mock('../src/lib/block');
+
+describe("Blockchain tests", () => {
+    
+    test('Should has genesis blocks', () => {
+        const blockchain = new Blockchain();
+        expect(blockchain.blocks.length).toEqual(1);
+    })
+
+    test('Should be valid', () => {
+        const blockchain = new Blockchain();
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should NOT be valid ', () => {
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block({
+            index: 1, 
+            previousHash: blockchain.blocks[0].hash, 
+            data: "Block 2"
+        } as Block));
+        blockchain.blocks[1].index = -1;
+        expect(blockchain.isValid().success).toEqual(false);
+    })
+
+    test('Should be valid (two blocks)', () => {
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block({
+            index: 1, 
+            previousHash: blockchain.blocks[0].hash, 
+            data: "Block 2"
+        } as Block));
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should add block', () => {
+        const blockchain = new Blockchain();
+        const result = blockchain.addBlock(new Block({
+            index: 1, 
+            previousHash: blockchain.blocks[0].hash, 
+            data: "Block 2"
+        } as Block));
+        expect(result.success).toEqual(true);
+    })
+
+    test('Should get block', () => {
+        const blockchain = new Blockchain();
+        const block = blockchain.getBlock(blockchain.blocks[0].hash);
+        expect(block).toBeTruthy();
+    })
+
+    test('Should NOT add block', () => {
+        const blockchain = new Blockchain();
+        const block = new Block({
+            index: -1, 
+            previousHash: blockchain.blocks[0].hash, 
+            data: "Block 2"
+        } as Block);
+        const result = blockchain.addBlock(block);
+        expect(result.success).toEqual(false);
+    })
+})
