a7c9ea742467208008d230f7c72e5d830c9f75ae
Cid Miranda <cidmiranda@gmail.com>
2024-02-29 11:17:05 -0300

    smart-contracts

---
+++ smart-contracts/BookDatabase.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract BookDatabase {
+    struct Book {
+        string isbn;
+        string title;
+        uint16 year;
+    }
+
+    uint32 private nextId = 0;
+    mapping (uint32 => Book) public books;
+
+    address private immutable owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function addBook(Book memory newBook) public {
+        nextId++;
+        books[nextId] = newBook;
+    }
+
+    function compare(string memory str1, string memory str2) private pure returns (bool) {
+        bytes memory arrA = bytes(str1);
+        bytes memory arrB = bytes(str2);
+        return arrA.length == arrB.length && keccak256(arrA) == keccak256(arrB);
+    }
+
+    function ediBook(uint32 id, Book memory newBook) public {
+        Book memory oldBook = books[id];
+
+        if(!compare(oldBook.title, newBook.title) && !compare(newBook.title, ""))
+            books[id].title = newBook.title;
+
+        if(!compare(oldBook.isbn, newBook.isbn) && !compare(newBook.isbn, ""))
+            books[id].isbn = newBook.isbn;
+        
+        if(oldBook.year != newBook.year && newBook.year > 0)
+            books[id].year = newBook.year;
+    }
+
+    function removeBook(uint32 id) public restricted {
+        delete books[id];
+    }
+
+    modifier restricted() {
+        require(owner == msg.sender, "You don't have permission.");
+        _;
+    }
+}
---
+++ smart-contracts/JoKenPo.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+//Módulo 02 > Lição 03 > Tóps 08 e 09 - Contrato JoKenPo
+contract JoKenPo {
+
+    enum Options { NONE, ROCK, PAPER, SCISSORS }//0, 1, 2, 3
+
+    Options private choice1 = Options.NONE;
+    address private player1;
+    string public result = "";
+
+    address payable private immutable owner;
+
+    struct Player {
+        address wallet;
+        uint32 wins;
+    }
+
+    Player[] public players;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function secretFunction() public view returns (string memory) {
+        require(owner==msg.sender, "You don't have permisssions to do this.");
+        return "olele";
+    }
+
+    function updateWinner(address winner) private {
+        for(uint i = 0; i < players.length; i++){
+            if(players[i].wallet == winner){
+                players[i].wins++;
+                return;
+            }
+        }
+        players.push(Player(winner, 1));
+    }
+
+    function finishGame(string memory newResult, address winner) private {
+        address contractAdrress = address(this);
+        payable(winner).transfer((contractAdrress.balance / 100) * 90);
+        owner.transfer(contractAdrress.balance);
+
+        updateWinner(winner);
+
+        result = newResult;
+        player1 = address(0);
+        choice1 = Options.NONE;
+    }
+
+    function getBalance() public view returns (uint) {
+        require(owner == msg.sender, "You don't have this permisssion.");
+        return address(this).balance;
+    }
+
+    function play(Options newChoice) public payable {
+        require(newChoice != Options.NONE, "Invalid choice");
+        require(player1 != msg.sender, "Wait the another player.");
+        require(msg.value >= 0.01 ether, "Invalid bid.");
+
+        if(choice1 == Options.NONE){
+            player1 = msg.sender;
+            choice1 = newChoice;
+            result = "Player 1 choose his/her option. Waiting player 2.";
+        }
+        else if(choice1 == Options.ROCK && newChoice == Options.SCISSORS)
+            finishGame("Rock breaks scissors. Player 1 won.", player1);
+        else if(choice1 == Options.PAPER && newChoice == Options.ROCK)
+            finishGame("Paper wraps rock. Player 1 won.", player1);
+        else if(choice1 == Options.SCISSORS && newChoice == Options.PAPER)
+            finishGame("Scissors cuts paper. Player 1 won.", player1);
+        else if(choice1 == Options.SCISSORS && newChoice == Options.ROCK)
+            finishGame("Rock breaks scissors. Player 2 won.", msg.sender);
+        else if(choice1 == Options.ROCK && newChoice == Options.PAPER)
+            finishGame("Paper wraps rock. Player 2 won.", msg.sender);
+        else if(choice1 == Options.PAPER && newChoice == Options.SCISSORS)
+            finishGame("Scissors cuts paper. Player 2 won.", msg.sender);
+        else{
+            result = "Draw game. The prize was doubled.";
+            player1 = address(0);
+            choice1 = Options.NONE;
+        }
+    }
+
+    function getLeaderBoard() public view returns (Player[] memory) {
+        if(players.length < 2) return players;
+
+        Player[] memory arr = new Player[](players.length);
+        for(uint i = 0; i < players.length; i++)
+            arr[i] = players[i];
+
+        for(uint i = 0; i < arr.length - 1; i++){
+            for(uint j = 0; j < arr.length; j++){
+                if(arr[i].wins < arr[j].wins){
+                    Player memory change = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = change;
+                }
+            }
+        }
+        return arr;
+    }
+}
