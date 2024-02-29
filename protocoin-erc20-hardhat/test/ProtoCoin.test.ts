c82bea042cb4cd1027ca46bea824bbac88bff787
Cid Miranda <cidmiranda@gmail.com>
2024-02-29 11:35:29 -0300

    setup inicial do projeto

---
+++ protocoin-erc20-hardhat/test/ProtoCoin.test.ts
@@ -0,0 +1,24 @@
+import {
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("ProtoCoin", function () {
+  
+  async function deployFixture() {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const ProtoCoin = await ethers.getContractFactory("ProtoCoin");
+    const protoCoin = await ProtoCoin.deploy();
+
+    return { protoCoin, owner, otherAccount };
+  }
+
+  it("Should test", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+
+    expect(true).to.equal(true);
+  });
+
+});
